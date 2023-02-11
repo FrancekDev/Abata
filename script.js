0d87f461c94a4812a2a86bd73b8558900041e635
mikrovalnapecnica <franmiljkovic7@gmail.com>
2023-02-11 17:14:28 +0100

    Polishing

--- script.js
+++ script.js
@@ -6,14 +6,9 @@ const submit = document.getElementById("contact_form");
 const button = document.getElementById("button");
 
 const hamburger = document.querySelector(".hamburger");
+const hamburgerMenu = document.querySelector(".hamburger__bg");
+const section = document.querySelector(".section");
 
-/*
-const from_name = document.getElementById("from_name");
-const phone_num = document.getElementById("phone_num").value;
-const email_id = document.getElementById("email_id").value;
-const company_name = document.getElementById("company_name").value;
-const message = document.getElementById("message").value;
-*/
 /* --------------- Sticky Navbar ---------------- */
 
 function stickyNavbar() {
@@ -26,14 +21,21 @@ window.addEventListener("scroll", stickyNavbar);
 
 /* --------------- Hamburger Menu Form ---------------- */
 
+let isOpen = false;
+
+if ((document.body.classList = "open")) {
+  isOpen = true;
+}
+
 hamburger.addEventListener("click", () => {
   document.body.classList.toggle("open");
-  document.body.classList.toggle("close");
+});
+
+hamburgerMenu.addEventListener("click", () => {
+  document.body.classList.remove("open");
 });
 
 /* --------------- emailJS Form ---------------- */
-
-/*-----> RADI ali ne rquired <-----*/
 
 button.addEventListener("click", function () {
   const fromName = document.getElementById("from_name");
@@ -52,9 +54,9 @@ button.addEventListener("click", function () {
 
   if (isFormValid) {
     let params = {
-      from_name: document.getElementById("from_name").value,
-      phone_num: document.getElementById("phone_num").value,
-      email_id: document.getElementById("email_id").value,
+      from_name: fromName.value,
+      phone_num: phoneNum.value,
+      email_id: email.value,
       company_name: document.getElementById("company_name").value,
       message: document.getElementById("message").value,
     };
@@ -65,25 +67,3 @@ button.addEventListener("click", function () {
       });
   }
 });
-
-/* RADI ali ne šalje vrijednosti
-let params = {
-  from_name: document.getElementById("from_name").value,
-  phone_num: document.getElementById("phone_num").value,
-  email_id: document.getElementById("email_id").value,
-  company_name: document.getElementById("company_name").value,
-  message: document.getElementById("message").value,
-};
-
-submit.addEventListener("click", function () {
-  if (from_name.checkValidity()) {
-    console.log("sending");
-
-    emailjs
-      .send("personal_email", "template_lar1gag", params)
-      .then(function () {
-        alert(`Poslano!`);
-      });
-  }
-});
-*/
